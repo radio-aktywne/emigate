1c7e97132b675c3949f380f9f38d05579d8876c2
Sebastian Pietras <spietras1998@gmail.com>
2022-09-07 03:49:21 +0200

    Enabled auth header forwarding (#16)

--- emigate/start.sh
+++ emigate/start.sh
@@ -4,6 +4,8 @@ cat <<EOF >./traefik.yml
 entryPoints:
   http:
     address: ":${EMIGATE_PORT:-12000}"
+    forwardedHeaders:
+      trustedIPs: "${EMIGATE_TRUSTED_IPS:-127.0.0.1}"
   udp:
     address: ":${EMIGATE_PORT:-12000}/udp"
 
