585bd3cb7ca89341c3a81e412ce2c8cbc69a56ed
Sebastian Pietras <spietras1998@gmail.com>
2022-09-08 10:18:58 +0200

    Moved to HTTPS for `authgate` (#17)

--- emigate/start.sh
+++ emigate/start.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+until [ -f /etc/certs/backend/ca.pem ]; do
+  echo "Waiting for backend certificates..."
+  sleep 1
+done
+
+cat /etc/certs/backend/ca.pem >>/etc/ssl/certs/ca-certificates.crt
+
 cat <<EOF >./traefik.yml
 entryPoints:
   http:
@@ -8,6 +15,10 @@ entryPoints:
       trustedIPs: "${EMIGATE_TRUSTED_IPS:-127.0.0.1}"
   udp:
     address: ":${EMIGATE_PORT:-12000}/udp"
+
+serversTransport:
+  rootCAs:
+    - /etc/certs/backend/ca.pem
 
 api:
   dashboard: true
